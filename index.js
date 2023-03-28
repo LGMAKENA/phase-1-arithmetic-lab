2b16e6ffcec090bdeb06e0313680ee371fe7b808
lgmakena <linetmakenag@gmail.com>
2023-03-28 16:14:37 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) + 1;
+// math.floor gives a figure lower than the rounded off figgure in a case of decimals ef 8.456 math.flooe will be 8 if math.cel will be 9
+let num3 = 9
+let num4 = 5
+let mod = num3%num4
+// the % is a modulo....a modulo gives reminder eg in that case, 9/5 has only one five and 4 is the remainder.
+
+let max;
+max = Math.max(10,20);
